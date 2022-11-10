6e58512d39a9bbc103f5630fb54e0c5e4897562c
ViktorLukashevich <101119183+lvktm@users.noreply.github.com>
2022-11-10 14:22:37 +0300

    Добавит моковые данные и маршрутизацию

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,10 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import { AppRoute } from '../../const';
 import Main from '../../pages/main/main';
+import Login from '../../pages/login/login';
+import Property from '../../pages/property/property';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
+
 
 type AppMainProps = {
   rentCount: number;
@@ -6,7 +12,26 @@ type AppMainProps = {
 
 function App({rentCount}: AppMainProps): JSX.Element {
   return (
-    <Main rentCount={rentCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<Main rentCount={rentCount} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<Login />}
+        />
+        <Route
+          path={AppRoute.Property}
+          element={<Property />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundPage />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
