b0b903ee5f1eaa105723cf6913a4b08c41756e94
ViktorLukashevich <101119183+lvktm@users.noreply.github.com>
2022-12-11 17:45:20 +0300

    6.1. Преобразует ссылки на иконки маркеров в перечисление и сделает удаление маркеров на карте после демонтирования компонента

--- project/src/const.ts
+++ project/src/const.ts
@@ -17,6 +17,7 @@ export const REVIEW_RATING_STARS = [
   { title: 'terribly', value: 1 }
 ];
 
-export const URL_MARKER_DEFAULT = 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
-
-export const URL_MARKER_CURRENT = 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
+export enum IconMarker {
+  Default = './img/pin.svg',
+  Active = './img/pin-active.svg'
+}
